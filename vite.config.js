8259ed9dae5dd8e842c18dd1795504a8d210126a
TruffleCod <angelzhu2333@gmail.com>
2026-07-17 16:59:42 +0800

    Simplify base path configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/heartshome/' : '/',
+export default defineConfig({
+  base: '/',
   build: {
     sourcemap: false,
   },
   plugins: [react()],
-}))
+})
